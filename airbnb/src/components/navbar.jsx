f28639297b8269d56ac16c41bd82df0a2b294f84
fidelia <fidelialtm13@gmail.com>
2024-01-19 15:28:05 +0200

    react

---
+++ airbnb/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './style.css'
+import download from '../download.jpeg'
+
+
+function Navbar(){
+    return(
+
+        <div>
+        <img src={download}/>
+        
+        </div>
+       
+
+    )
+}
+
+export default Navbar;
+
