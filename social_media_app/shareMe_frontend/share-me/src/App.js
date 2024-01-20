3c03f67ba166bf951e90b7f602ad31830c6a7225
fidelia <fidelialtm13@gmail.com>
2024-01-21 00:20:51 +0200

    routes

--- social_media_app/shareMe_frontend/share-me/src/App.js
+++ social_media_app/shareMe_frontend/share-me/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Login from "./components/login";  // Assuming the correct import path
+import Home from "./container/home";  // Assuming the correct import path
 
-const App= ()=>{
-    return(
-        <div>Hello World</div>
-    )
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
