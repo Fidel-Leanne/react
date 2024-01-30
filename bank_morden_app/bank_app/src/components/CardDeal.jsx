63631808e5aadc48959fda3c5294459cd65e2b03
fidelia <fidelialtm13@gmail.com>
2024-01-30 23:22:11 +0200

    done

--- bank_morden_app/bank_app/src/components/CardDeal.jsx
+++ bank_morden_app/bank_app/src/components/CardDeal.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { card } from "../assets/assets"
+import styles , {layout} from "../style"
+import Button from './Button'
 
-const CardDeal = () => {
-  return (
-    <div>
-      CardDeal
-    </div>
+const CardDeal = () => (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}> Find a better card deal <br className="sm:block hidden"/>
+        in a few easy steps
+        </h2>
+
+        <p className={`${styles.paragraph} max-w-[470px mt-5]`}>Explore our exclusive card deals and elevate your financial 
+            experience with [Your Bank Name]. Whether you're looking for premium rewards, 
+            low-interest rates, or exclusive perks, our range of credit and debit cards
+            caters to diverse financial needs. 
+        </p>  
+
+      <Button styles='mt-5'/>
+
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+      </div>
+
+    
+    </section>
   )
-}
+
 
 export default CardDeal
--- bank_morden_app/bank_app/src/components/Clients.jsx
+++ bank_morden_app/bank_app/src/components/Clients.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { clients } from "../constants"
+import styles from "../style"
 
-const Clients = () => {
-  return (
-    <div>
-      Clients
+const Clients=()=>(
+
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+    {clients.map((client)=>(
+      <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] cursor-pointer hover:glow`}>
+       <img src={client.logo} alt="client"
+       className="sm:w-[192px] w-[100px] object-contain image"/>
+      <div/></div>
+    ))}
     </div>
-  )
-}
+  </section>
+)
+
 
 export default Clients
