0e0a14fe6362099b966d942e2f37598296890398
fidelia <fidelialtm13@gmail.com>
2024-02-16 16:10:00 +0200

    shareMe app initialisation

---
+++ shareMe/shareme_backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'acmwirz3',
+    dataset: 'production'
+  }
+})
---
+++ shareMe/shareme_backend/schemaTypes/index.ts
@@ -0,0 +1,8 @@
+import user from './user'
+import postedby from './postedby'
+import save from './save'
+import comment from './comment'
+import pin from './pin'
+
+
+export const schemaTypes = [user, pin, comment, save,postedby]
